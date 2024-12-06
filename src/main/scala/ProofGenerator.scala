63147b767d55de77b60067a7427612fa2938a1ea
achoowy <achoowy@gmail.com>
2024-12-06 15:24:57 -0700

    Changes

--- src/main/scala/ProofGenerator.scala
+++ src/main/scala/ProofGenerator.scala
@@ -1,5 +1,15 @@
 class ProofGenerator {
-  sealed trait Expr
+  sealed trait Expr {
+    override def toString: String = this match {
+      case Variable(n)          => n
+      case Not(e)               => "!$e"
+      case And(e1, e2)          => "(" + e1 + "∧" + e2 + ")"
+      case Or(e1, e2)           => "($e1 ∨ $e2)"
+      case Implies(e1, e2)      => "($e1 → $e2)"
+      case Parenthesis(e1)      => "($e1)"
+    }
+  }
+
   case class Variable(n: String) extends Expr
   case class Not(e: Expr) extends Expr
   case class And(e1: Expr, e2: Expr) extends Expr
@@ -7,17 +17,17 @@ class ProofGenerator {
   case class Implies(e1: Expr, e2: Expr) extends Expr
   case class Parenthesis(e1: Expr) extends Expr
 
-  sealed trait Value
+  sealed trait Value {
+    override def toString: String = this match {
+      case Boolean(n) => n
+    }
+  }
   case class Boolean(n: String) extends Value
 
   sealed trait ErrorValue extends Value
   case object InvalidDivByZero extends ErrorValue
   case object InvalidOdd extends ErrorValue
 
-
-  def parseStatement(s: String): Expr = {
-    Variable("A");
-  }
 
 
 //  def eval(e: Expr): Value = {
--- src/test/scala/ProofGeneratorTest.scala
+++ src/test/scala/ProofGeneratorTest.scala
@@ -4,18 +4,7 @@ class ProofGeneratorTest extends AnyFunSuite {
   val generator = new ProofGenerator()
   import generator._  // This imports all members from ProofGenerator instance
   
-  test("Basic grammar construction") {
-    assert(eval(Const(1)) == InvalidOdd)
-    assert(eval(Const(1.5)) == NumValue(1.5))
-    assert(eval(Const(2)) == NumValue(2))
-    // Neg Tests
-    assert(eval(Neg(Const(2))) == NumValue(-2))
-    assert(eval(Neg(Const(1))) == InvalidOdd)
-    // Add Tests
-    assert(eval(Plus(Const(2), Const(4))) == NumValue(6))
-    assert(eval(Plus(Const(1), Const(4))) == InvalidOdd)
-    assert(eval(Plus(Const(12), Const(5))) == InvalidOdd)
-    //
-    eval(Minus(Mult(Const(65), Const(2)), Const(61)))
+  test("Test Expression to String") {
+    System.out.println(And(Variable("a"),(Variable("b"))))
   }
 }
