ef14b67449d2eb007c9a266c61a4bfb07b106395
Yash Thapliyal <yash.thapliyal.007@gmail.com>
2024-12-09 19:24:09 -0700

    Defined Equality in State

--- src/main/scala/Math/State.scala
+++ src/main/scala/Math/State.scala
@@ -1,5 +1,15 @@
 package Math
 
 class State(expr: Expr, operation: Operation) {
+  val children: Seq[State] = Vector.empty[State]
 
+  override def equals(obj: Any): Boolean = obj match {
+    case that: State =>
+      this.getExpr() == that.getExpr()
+    case _ => false
+  }
+
+  def getExpr(): Expr = {
+    return this.expr
+  }
 }
