6912490bacf8198666673aa3884bd98efe47aa1f
Yash Thapliyal <yash.thapliyal.007@gmail.com>
2024-12-09 20:07:56 -0700

    Proof generator testing

--- src/test/scala/ProofGeneratorTest.scala
+++ src/test/scala/ProofGeneratorTest.scala
@@ -9,4 +9,21 @@ class ProofGeneratorTest extends AnyFunSuite {
     System.out.println(And(Parenthesis(Implies(And(Variable("a"), Variable("b")), Variable("c"))), Not(Variable("c"))))
     assert(And(Parenthesis(Implies(And(Variable("a"), Variable("b")), Variable("c"))), Not(Variable("c"))).toString() == "(a∧b→c)∧!c")
   }
+
+  test("Test Pass in Theorems") {
+    val t1 = Theorem(Not(And(Variable("a"), Variable("b"))), Or(Not(Variable("a")), Not(Variable("b"))))
+    //    System.out.println(t1.getAntecedent())
+    //    System.out.println(t1.getConsequent())
+
+    val expr1 = And(Implies(And(Variable("a"), Variable("b")), Variable("c")), Not(Variable("c")))
+
+    expr1 match {
+      case And(e1, e2)  => {
+        System.out.println(e1)
+        System.out.println(e2)
+      }
+    }
+  }
+
+
 }
