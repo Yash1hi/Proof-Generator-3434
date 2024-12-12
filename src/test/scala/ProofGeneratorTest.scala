7aa16f733abd482dfcf931a17cc1f283a7788c47
achoowy <achoowy@gmail.com>
2024-12-11 18:00:44 -0700

    Test.

--- src/test/scala/ProofGeneratorTest.scala
+++ src/test/scala/ProofGeneratorTest.scala
@@ -12,16 +12,25 @@ class ProofGeneratorTest extends AnyFunSuite {
   }
 
   test("Test Pass in Theorems") {
+    // De Morgan's Law
     val t1 = Theorem(Not(And(Variable("a"), Variable("b"))), Or(Not(Variable("a")), Not(Variable("b"))))
+    // Transitive Property of And
     val t2 = Theorem(And(Variable("a"), Variable("b")), And(Variable("b"), Variable("a")))
-    System.out.println("antecedent = " + t1.getAntecedent)
-    System.out.println("consequent = " + t1.getConsequent)
+    // Not is distributive over And
+    val t3 = Theorem(Not(And(Variable("a"), Variable("b"))), And(Not(Variable("a")), Not(Variable("b"))))
+    // Modus Tollens
+    val t4 = Theorem(Implies(Variable("a"), Variable("b")), Implies(Not(Variable("b")), Not(Variable("a"))))
+    // Implication Elimination
+    val t5 = Theorem(And(Variable("a"), Implies(Variable("a"), Variable("b"))), Variable("b"))
+
+    //    System.out.println("antecedent = " + t1.getAntecedent)
+    //    System.out.println("consequent = " + t1.getConsequent)
 
     val expr1 = And(Implies(And(Variable("a"), Variable("b")), Variable("c")), Not(Variable("c")))
     val expr2 = And(Variable("c"), Variable("b"))
     val expr3 = And(And(Variable("y"), Variable("z")), And(Variable("c"), Variable("d")))
 
-    System.out.println("expression = " + expr1)
+    //    System.out.println("expression = " + expr1)
 
     val applicableExpressions: Seq[(Expr, Expr)] = t2.getApplicable(expr1)
     applicableExpressions.foreach((e1: Expr, e2: Expr) => {
@@ -29,6 +38,34 @@ class ProofGeneratorTest extends AnyFunSuite {
       println("Result: " + e2)
     })
 
+    val theorems = Vector(t1, t2, t3, t4, t5)
+
+    val startState = State(expr1, null)
+    startState.expandState(theorems)
+    for (child1 <- startState.children) {
+      child1.expandState(theorems)
+      println()
+      if (And(Not(Variable("c")), Implies(And(Variable("a"), Variable("b")), Variable("c"))) == child1.getExpr()) {
+        for (child2 <- child1.children) {
+          child2.expandState(theorems)
+          for (child3 <- child2.children) {
+            child3.expandState(theorems)
+            for (child4 <- child3.children) {
+              child4.expandState(theorems).foreach((leaf: State) => {
+                val up1 = leaf.getOperation().getPreviousState
+                val up2 = up1.getOperation().getPreviousState
+                val up3 = up2.getOperation().getPreviousState
+                val up4 = up3.getOperation().getPreviousState
+                val up5 = up4.getOperation().getPreviousState
+                //              if (up2.getExpr() == Not(And(Variable("a"), Variable("b")))) {
+                //                System.out.println("Leaf:" + up5.getExpr() + " -> " + up4.getExpr() + " -> " + up3.getExpr() + " -> " + up2.getExpr() + " -> " + up1.getExpr() + " -> " + leaf.getExpr())
+                //              }
+              })
+            }
+          }
+        }
+      }
+    }
   }
 }
 
