37d51e5bd63c06740607b7ea9c369c3fef46d813
Yash Thapliyal <yash.thapliyal.007@gmail.com>
2024-12-12 06:48:44 -0700

    Added extra examples

--- src/test/scala/ProofGeneratorTest.scala
+++ src/test/scala/ProofGeneratorTest.scala
@@ -6,72 +6,33 @@ class ProofGeneratorTest extends AnyFunSuite {
 
   import generator._ // This imports all members from ProofGenerator instance
 
-  test("Test Expression to String") {
-    System.out.println(And(Implies(And(Variable("a"), Variable("b")), Variable("c")), Not(Variable("c"))))
-    assert(And(Implies(And(Variable("a"), Variable("b")), Variable("c")), Not(Variable("c"))).toString() == "(a∧b→c)∧!c")
+
+
+  test("Example BFS Proof 1") {
+    // Modus Ponens
+    val t1 = Theorem(And(Variable("a"), Implies(Variable("a"), Variable("b"))), Variable("b"), "Modus Ponens")
+    // Transitive Property of Or
+    val t2 = Theorem(Or(Variable("a"), Variable("b")), Or(Variable("b"), Variable("a")), "Transitive Property of Or")
+    // Distributive Property
+    val t3 = Theorem(And(Variable("a"), Or(Variable("b"), Variable("c"))), Or(And(Variable("a"), Variable("b")), And(Variable("a"), Variable("c"))), "Distributive Property")
+    // Double Negation
+    val t4 = Theorem(Not(Not(Variable("a"))), Variable("a"), "Double Negation")
+
+    val premise = And(Variable("a"), Implies(Variable("a"), Not(Not(Variable("p"))))) 
+    val conclusion = Variable("p")
+
+    val theorems = Vector(t1, t2, t3, t4)
+    println(premise)
+    println(conclusion)
+
+    val startState = State(premise, null)
+    val prover = Prover(theorems, premise, conclusion)
+    val foundState = prover.bfs()
+    println(foundState.getProof())
+
   }
 
-//  test("Test Pass in Theorems") {
-//    // De Morgan's Law
-//    val t1 = Theorem(Not(And(Variable("a"), Variable("b"))), Or(Not(Variable("a")), Not(Variable("b"))))
-//    // Transitive Property of And
-//    val t2 = Theorem(And(Variable("a"), Variable("b")), And(Variable("b"), Variable("a")))
-//    // Modus Tollens
-//    val t4 = Theorem(Implies(Variable("a"), Variable("b")), Implies(Not(Variable("b")), Not(Variable("a"))))
-//    // Implication Elimination
-//    val t5 = Theorem(And(Variable("a"), Implies(Variable("a"), Variable("b"))), Variable("b"))
-//
-//    System.out.println("antecedent = " + t5.getAntecedent)
-//    System.out.println("consequent = " + t5.getConsequent)
-//
-//    val expr1 = And(Implies(And(Variable("a"), Variable("b")), Variable("c")), Not(Variable("c")))
-//    val expr2 = And(Variable("c"), Variable("b"))
-//    val expr3 = And(And(Variable("y"), Variable("z")), And(Variable("c"), Variable("d")))
-//    val expr4 = And(Variable("a"), Implies(Variable("a"), Variable("b")))
-//    //    System.out.println("expression = " + expr1)
-//
-//    val applicableExpressions: Seq[(Expr, Expr)] = t5.getApplicable(expr4)
-//    applicableExpressions.foreach((e1: Expr, e2: Expr) => {
-//      println("Theorem applied to: " + e1)
-//      println("Result: " + e2)
-//    })
-//
-//    val theorems = Vector(t1, t2, t4, t5)
-//    println(theorems.last.getAntecedent)
-//
-//    val startState = State(expr1, null)
-//    startState.expandState(theorems)
-//    for (child1 <- startState.children) {
-//      child1.expandState(theorems)
-//      if (And(Not(Variable("c")), Implies(And(Variable("a"), Variable("b")), Variable("c"))) == child1.getExpr()) {
-//        System.out.println("child1: " + child1.getExpr())
-//        for (child2 <- child1.children) {
-//          child2.expandState(theorems)
-//          if (And(Not(Variable("c")), Implies(Not(Variable("c")), Not(And(Variable("a"), Variable("b"))))) == child2.getExpr()) {
-//            System.out.println("child2: " + child2.getExpr())
-//            for (child3 <- child2.children) {
-//              child3.expandState(theorems)
-//              System.out.println("child3: " + child3.getExpr())
-//              for (child4 <- child3.children) {
-//                child4.expandState(theorems).foreach((leaf: State) => {
-//                  val up1 = leaf.getOperation().getPreviousState
-//                  val up2 = up1.getOperation().getPreviousState
-//                  val up3 = up2.getOperation().getPreviousState
-//                  val up4 = up3.getOperation().getPreviousState
-//                  val up5 = up4.getOperation().getPreviousState
-//                  if (up2.getExpr() == Not(And(Variable("a"), Variable("b")))) {
-//                    System.out.println("Leaf:" + up5.getExpr() + " -> " + up4.getExpr() + " -> " + up3.getExpr() + " -> " + up2.getExpr() + " -> " + up1.getExpr() + " -> " + leaf.getExpr())
-//                  }
-//                })
-//              }
-//            }
-//          }
-//        }
-//      }
-//    }
-//  }
-
-  test("Example BFS Proof") {
+  test("Example BFS Proof 2") {
     // De Morgan's Law
     val t1 = Theorem(Not(And(Variable("a"), Variable("b"))), Or(Not(Variable("a")), Not(Variable("b"))), "De Morgan's Law")
     // Transitive Property of And
@@ -95,7 +56,32 @@ class ProofGeneratorTest extends AnyFunSuite {
 
   }
 
-  test("Cantor's Diagonal Argument") {
+  test("Example BFS Proof 3") {
+    // De Morgan's Law
+    val t1 = Theorem(Not(And(Variable("a"), Variable("b"))), Or(Not(Variable("a")), Not(Variable("b"))), "De Morgan's Law")
+    val t3 = Theorem(Or(Not(Variable("a")), Not(Variable("b"))), Not(And(Variable("a"), Variable("b"))), "De Morgan's Law")
+    // Transitive Property of And
+    val t2 = Theorem(And(Variable("a"), Variable("b")), And(Variable("b"), Variable("a")), "Transitive Property of And")
+    // Modus Tollens
+    val t4 = Theorem(Implies(Variable("a"), Variable("b")), Implies(Not(Variable("b")), Not(Variable("a"))), "Modus Tollens")
+    // Implication Elimination
+    val t5 = Theorem(And(Variable("a"), Implies(Variable("a"), Variable("b"))), Variable("b"), "Implication Elimination")
+    val t6 = Theorem(Not(Not(Variable("a"))), Variable("a"), "Double Negation")
+
+    val premise = And(Implies(And(Variable("Cloudy"), Variable("Rainy")), Variable("Umbrella")), Not(Or(Not(Variable("Rainy")), Not(Variable("Cloudy")))))
+    val conclusion = Variable("Umbrella")
+
+
+    val theorems = Vector(t1, t2, t3, t4, t5, t6)
+    println(premise)
+
+     val startState = State(premise, null)
+     val prover = Prover(theorems, premise, conclusion)
+     val foundState = prover.bfs()
+     println(foundState.getProof())
+
   }
+
+  
 }
 
